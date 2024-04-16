d77aa2a4188711d8248e0ab0bdf81736f7f13183
RafaRissatto <rafaelrissatto9@gmail.com>
2024-04-16 15:33:20 -0300

    setup modificado no package

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "Teste"
+PACKAGE = "Teste2"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Rafael Rissatto"
