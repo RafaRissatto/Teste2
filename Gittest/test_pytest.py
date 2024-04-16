343ba8c033b8a7e831567ce97c71e5e96b5192bd
RafaRissatto <rafaelrissatto9@gmail.com>
2024-04-16 15:49:04 -0300

    novo arquivo de teste

--- Gittest/test_pytest.py
+++ Gittest/test_pytest.py
@@ -1,4 +1,11 @@
 import requests
 
-def teste_buscar_avatar():
+
+def teste_buscar_avatar(usuario):
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+
+def test_answer():
     assert teste_buscar_avatar('RafaRissatto') == 'https://avatars.githubusercontent.com/u/149025114?v=4'
