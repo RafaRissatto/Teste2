cb061e824d3877a05dc4be81db61cba877d39f3f
RafaRissatto <rafaelrissatto9@gmail.com>
2024-05-13 11:37:28 -0300

    alteraçoes para fazer o pack precisou mudar o nome do modulo

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "Gittest-para-package-R"
+PACKAGE = "appparabuscarfotodeperfilgit"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Rafael Rissatto"
